48e1780e4c34ec7b3ea4199fa9a5af4037b02d23
Brian Knauff <bkjr88@verizon.net>
2020-05-17 20:34:05 -0400

    First use of swing

--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -1,4 +1,12 @@
 package editor;
 
-public class TextEditor {
+import javax.swing.*;
+
+public class TextEditor extends JFrame {
+    public TextEditor() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+        setLayout(null);
+    }
 }
