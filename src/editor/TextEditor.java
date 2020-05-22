d5479b1ab48dc62c0f5acd273bf543ed470329d6
Brian Knauff <bkjr88@verizon.net>
2020-05-22 17:46:41 -0400

    Fixed loading and saving functionality and now works properly.
    Improvements needed to be made to code readability and neatness(but still does main purpose)

--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -3,7 +3,8 @@ package editor;
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
-import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class TextEditor extends JFrame{
     JTextArea textArea;
@@ -12,14 +13,19 @@ public class TextEditor extends JFrame{
     JScrollPane scroll;
 
     public TextEditor() {
-        getContentPane().setLayout(new FlowLayout());
         setSize(500, 500);
         setTitle("Text Editor");
-        textArea = new JTextArea(20, 35);
-        textField = new JTextField("", 15);
+        textArea = new JTextArea();
+        textArea.setSize(300,300);
+        textField = new JTextField();
+        textField.setSize(100, 30);
         sButton = new JButton("Save");
         lButton = new JButton("Load");
-        scroll = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scroll = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        JPanel input = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        input.add(textField);
+        input.add(sButton);
+        input.add(lButton);
 
         textArea.setName("TextArea");
         textField.setName("FilenameField");
@@ -28,35 +34,25 @@ public class TextEditor extends JFrame{
         scroll.setName("ScrollPane");
 
 
-        add(textField);
-        add(sButton);
-        add(lButton);
-        add(scroll);
+        add(textField, BorderLayout.NORTH);
+        add(input, BorderLayout.NORTH);
+        add(scroll, BorderLayout.CENTER);
 
         lButton.addActionListener(actionEvent -> {
-            String text = textField.getText();
-            File file = Paths.get(text).toFile();
-
             try {
-                FileReader stream = new FileReader(file);
-                StringBuilder str = new StringBuilder();
-                while (stream.ready()) {
-                    str.append((char)stream.read());
-                }
-                textField.setText(str.toString());
-            }catch (FileNotFoundException e){
-                textField.setText("");
+                textArea.setText(new String(Files.readAllBytes(Path.of(textField.getText()))));
             } catch (IOException e) {
+                textArea.setText(null);
                 e.printStackTrace();
             }
         });
 
         sButton.addActionListener(actionEvent -> {
-            File file = Paths.get(textField.getText()).toFile();
-            try (PrintWriter writer = new PrintWriter(file)) {
-                writer.print(textField.getText());
+            try (Writer writer = new FileWriter(new File(textField.getText()))) {
+                writer.write(textArea.getText());
             } catch (IOException e) {
-                e.getLocalizedMessage();
+                textArea.setText(null);
+                e.printStackTrace();
             }
         });
 
