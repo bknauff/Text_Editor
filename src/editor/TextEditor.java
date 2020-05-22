934e2512b27ca7dfd5b50aee7e74f710759b1781
Brian Knauff <bkjr88@verizon.net>
2020-05-22 17:00:56 -0400

    Added functionality for save and load buttons. Able to load in and save text files through TextArea.
    Change names to make identifying easier

--- src/editor/TextEditor.java
+++ src/editor/TextEditor.java
@@ -2,46 +2,65 @@ package editor;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.*;
+import java.nio.file.Paths;
 
-public class TextEditor extends JFrame implements ActionListener{
-    JTextArea ta;
-    JTextField f;
-    JButton s, l;
-    JFileChooser fileChooser = new JFileChooser(System.getProperty("user.fir"));
-    String curFile = "Untitled";
+public class TextEditor extends JFrame{
+    JTextArea textArea;
+    JTextField textField;
+    JButton sButton, lButton;
     JScrollPane scroll;
+
     public TextEditor() {
         getContentPane().setLayout(new FlowLayout());
         setSize(500, 500);
         setTitle("Text Editor");
-        ta = new JTextArea(20, 35);
-        f = new JTextField("",15);
-        s = new JButton("Save");
-        l = new JButton("Load");
-        scroll = new JScrollPane(ta, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        textArea = new JTextArea(20, 35);
+        textField = new JTextField("", 15);
+        sButton = new JButton("Save");
+        lButton = new JButton("Load");
+        scroll = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 
-        add(f);
-        add(s);
-        add(l);
-        add(scroll);
-        s.addActionListener(this);
-
-
-
-        ta.setName("TextArea");
-        f.setName("FilenameField");
-        s.setName("SaveButton");
-        l.setName("LoadButton");
+        textArea.setName("TextArea");
+        textField.setName("FilenameField");
+        sButton.setName("SaveButton");
+        lButton.setName("LoadButton");
         scroll.setName("ScrollPane");
 
 
+        add(textField);
+        add(sButton);
+        add(lButton);
+        add(scroll);
+
+        lButton.addActionListener(actionEvent -> {
+            String text = textField.getText();
+            File file = Paths.get(text).toFile();
+
+            try {
+                FileReader stream = new FileReader(file);
+                StringBuilder str = new StringBuilder();
+                while (stream.ready()) {
+                    str.append((char)stream.read());
+                }
+                textField.setText(str.toString());
+            }catch (FileNotFoundException e){
+                textField.setText("");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        sButton.addActionListener(actionEvent -> {
+            File file = Paths.get(textField.getText()).toFile();
+            try (PrintWriter writer = new PrintWriter(file)) {
+                writer.print(textField.getText());
+            } catch (IOException e) {
+                e.getLocalizedMessage();
+            }
+        });
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-    }
-    public void actionPerformed(ActionEvent e) {
-
     }
 }
